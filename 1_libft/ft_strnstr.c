59e90866305848eb2bbb4f3437d25602657a3332
seungb5 <seungbo1112@gmail.com>
2021-01-06 15:51:17 +0900

    Fix ft_strnstr

--- 1_libft/ft_strnstr.c
+++ 1_libft/ft_strnstr.c
@@ -11,39 +11,19 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-#include "stdio.h"
-#include "string.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
 	size_t l_len;
 
-	if (*big && *little)
-		return (NULL);
-	if (*big == '\0')
-		return (NULL);
 	if (*little == '\0')
 		return ((char *)big);
 	l_len = ft_strlen(little);
-	while (len-- >= l_len && *big != '\0')
+	while (*big && l_len <= len--)
 	{
-		if (*big == *little && ft_strncmp(big, little, l_len) == 0)
+		if (!ft_strncmp(big, little, l_len))
 			return ((char *)big);
 		big++;
 	}
 	return (NULL);
-}
-
-int		main(void)
-{
-	const char	*big = "12444";
-	const char	*little = "";
-	size_t		len = 3;
-	char		*res;
-	char		*ret;
-	res = ft_strnstr(big, little, len);
-	ret = strnstr(big, little, len);
-	printf("ft strn: %p \n", res);
-	printf("strn: %p \n", ret);
-	return 0;
 }
