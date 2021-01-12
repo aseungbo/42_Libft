5a9908c3d856304578045cb29b5aeee7c42f4909
seungb5 <seungbo1112@gmail.com>
2021-01-12 23:42:29 +0900

    20200112

--- 2_get_next_line/get_next_line.h
+++ 2_get_next_line/get_next_line.h
@@ -10,4 +10,11 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+# include <stdlib.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
